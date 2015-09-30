4856ace4dc04d51844dda69bc58a109fe5211832
luj96656 <jack.harper@stfc.ac.uk>
2015-09-30 10:27:04 +0100

    added rmtree function from shutil to delete temp files/directories

--- ceph-pg-analyst.py
+++ ceph-pg-analyst.py
@@ -10,6 +10,7 @@ import numpy  # imports the entire numpy module
 import matplotlib.pyplot as plt  # imports pyplot from matplotlib, calls at plt
 from imp import find_module  # imports find_module from imp
 import os
+from shutil import rmtree
 try:
     find_module('numpy')  # makes sure numpy is installed
     find_module('matplotlib')  # makes sure matplotlib is installed
